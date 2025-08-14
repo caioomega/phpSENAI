cfa37e8db7d646e2110fee6b4957f7bf91864485
caioomega <caio.mega@aluno.senai.br>
2025-08-14 15:43:33 -0300

    Atividades do dia 14/08/2025 - Quinta feira

---
+++ atividade_array.php
@@ -0,0 +1,18 @@
+<?php
+
+    $frutas = [
+     "Maçã <br>", 
+     "Banana <br> ",
+     "Laranja <br>",
+     "Limão <br>",
+     "Pera <br>",
+     "Morango <br>"];
+    
+    for($i = 0; $i <= 5; $i++){
+echo "fruta $i <br>";
+        echo $frutas [$i];
+}
+
+?>
+
+
